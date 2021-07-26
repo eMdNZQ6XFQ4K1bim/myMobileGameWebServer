0cc99dd9d712743d84f8172b14f6c7ecae4914f7
theGlayCoder <theglaycoder@gmail.com>
2021-07-26 15:00:51 +0900

    typeorm mysql db 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PW'),
+        database: configService.get('DB_NAME'),
+        entities: [join(__dirname, '/**/*.entity.js')],
+        synchronize: true,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
