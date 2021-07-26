42cdec3bc94924ce271ae42c160196c203198e66
theGlayCoder <theglaycoder@gmail.com>
2021-07-26 14:30:26 +0900

    검증 및 변환 관련 모듈 추가 및 설정 * class-validator * class-transformer * @nestjs/mapped-types * 글로벌 파이프라인 설정

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -19,6 +20,15 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, swaggerConfigs);
     SwaggerModule.setup('api', app, document);
   }
+
+  /**
+   * 전역 범위 파이프라인 설정
+   */
+  app.useGlobalPipes(new ValidationPipe({
+  	whitelist: true, // validation을 위한 decorator가 붙어있지 않은 속성들은 제거
+    forbidNonWhitelisted: true, // whitelist 설정을 켜서 걸러질 속성이 있다면 아예 요청 자체를 막도록 (400 에러)
+    transform: true, // 요청에서 넘어온 자료들의 형변환
+  }));
 
   await app.listen(3000);
 }
