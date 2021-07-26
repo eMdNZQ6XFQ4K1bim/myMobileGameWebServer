a8fb3b7d8fb2bade0e16959bff423b37906a9420
theGlayCoder <theglaycoder@gmail.com>
2021-07-26 14:04:33 +0900

    Swagger Module 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Swagger 사용 설정
+   */
+  if (process.env.SWAGGER_ENABLED === 'true') {
+    const swaggerConfigs = new DocumentBuilder()
+      .setTitle('my mobile web server API')
+      .setDescription('The my mobile web server API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfigs);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
